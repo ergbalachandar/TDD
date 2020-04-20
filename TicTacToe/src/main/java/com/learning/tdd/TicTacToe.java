694c31bb4bfd279786d09daad2a8ade6d9f32f20
Balachandar Gopalan <ergbalachandar@gmail.com>
2020-04-20 15:10:06 +0530

    Assigning symbol X for the playerOne

--- TicTacToe/src/main/java/com/learning/tdd/TicTacToe.java
+++ TicTacToe/src/main/java/com/learning/tdd/TicTacToe.java
@@ -2,5 +2,9 @@ package com.learning.tdd;
 
 public class TicTacToe {
 	
+	public String placeSymbol(int position) {
+		return "X";
+	}
+	
 
 }
--- TicTacToe/src/test/java/com/learning/tdd/TicTacToeTest.java
+++ TicTacToe/src/test/java/com/learning/tdd/TicTacToeTest.java
@@ -1,5 +1,14 @@
 package com.learning.tdd;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
 public class TicTacToeTest {
+	@Test
+	public void firstPlayerShouldAssignSymbolX() {
+		TicTacToe ticTacToe = new TicTacToe();
+		int position = 1;
+	    Assertions.assertEquals("X", ticTacToe.placeSymbol(position));
+	}
 
 }
