ec13f855186a49acbb0aad02ba260fde38c69959
Balachandar Gopalan <ergbalachandar@gmail.com>
2020-04-20 15:27:42 +0530

    Assigning symbol O for the playerTwo

--- TicTacToe/src/main/java/com/learning/tdd/TicTacToe.java
+++ TicTacToe/src/main/java/com/learning/tdd/TicTacToe.java
@@ -2,8 +2,15 @@ package com.learning.tdd;
 
 public class TicTacToe {
 	
-	public String placeSymbol(int position) {
-		return "X";
+	public String placeSymbol(int position,String user) {
+		String symbol = " ";
+		if(user.equalsIgnoreCase("firstPlayer")) {
+		symbol =  "X";
+		}
+		else if(user.equalsIgnoreCase("secondPlayer")) {
+			symbol =  "O";
+		}
+		return symbol;
 	}
 	
 
--- TicTacToe/src/test/java/com/learning/tdd/TicTacToeTest.java
+++ TicTacToe/src/test/java/com/learning/tdd/TicTacToeTest.java
@@ -2,13 +2,30 @@ package com.learning.tdd;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+
 
 public class TicTacToeTest {
+	
+	private TicTacToe ticTacToe;
+	
+	@BeforeEach
+	public void setUp() throws Exception 
+	{
+	   ticTacToe = new TicTacToe();    		
+	} 
+	
+	
 	@Test
 	public void firstPlayerShouldAssignSymbolX() {
-		TicTacToe ticTacToe = new TicTacToe();
 		int position = 1;
-	    Assertions.assertEquals("X", ticTacToe.placeSymbol(position));
+	    Assertions.assertEquals("X", ticTacToe.placeSymbol(position,"firstPlayer"));
+	}
+	
+	@Test
+	public void secondPlayerShouldAssignSymbolO() {
+		int secondPosition = 2;
+         Assertions.assertEquals("O",ticTacToe.placeSymbol(secondPosition, "SecondPlayer"));
 	}
 
 }
